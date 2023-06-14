560facf85495bf4e601ba8998bd94045079fad88
huydsai02 <huydsai02@gmail.com>
2023-06-15 00:35:21 +0700

    Temp update

--- app_fa.py
+++ app_fa.py
@@ -7,7 +7,7 @@ import sounddevice as sd
 from map_color import map_color
 
 # url = "http://127.0.0.1:8000"
-url = 'https://4746-34-124-155-136.ngrok-free.app'
+url = 'https://9a3a-34-122-212-11.ngrok-free.app'
 current_folder = os.path.dirname(os.path.realpath(__file__))
 img_folder = os.path.join(current_folder, 'img')
 audio_folder = os.path.join(current_folder, 'audio')
--- map_color.py
+++ map_color.py
@@ -1,11 +1,18 @@
 def map_color(score_list, start_index=1) -> dict:
     list_result = list()
+    print(score_list)
     for word in score_list:
         for right_phoneme, model_predict, score in word:
             if right_phoneme == model_predict:
-                tag = "right"
+                if score >= 0.4:
+                    tag = "right"
+                else:
+                    tag = 'neutral'
             else:
-                tag = 'wrong' # 'neutral'
+                if score >= 0.1:
+                    tag = 'wrong'
+                else:
+                    tag = 'neutral' # 'neutral'
             list_result.append((right_phoneme, tag))
         list_result.append((" ", 'normal'))
     if len(list_result) > 0:
