72ffa99210c6e9d73f84dad806856d8e32c018c5
huydsai02 <huydsai02@gmail.com>
2023-06-15 17:46:59 +0700

    Update

---
+++ model/al_batch/main.py
@@ -0,0 +1,4 @@
+from api_fa import run_api
+
+auth_tok = 'your ngrok auth token'
+run_api(auth_tok, 'ap')
---
+++ model/al_batch/model.py
@@ -0,0 +1,32 @@
+import torch.nn as nn
+import torch
+from encoder import AcousticEncoder, LinguisticEncoder
+from decoder import Decoder
+
+class AL(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.acoustic_encoder   = AcousticEncoder(81, 768)
+        self.linguistic_encoder  = LinguisticEncoder(768, vocab_size)
+        self.decoder            = Decoder(768, vocab_size)
+
+    def forward(self, acoustic, linguistic):
+        Hq      = self.acoustic_encoder(acoustic)
+        Hk, Hv  = self.linguistic_encoder(linguistic)
+        logits  = self.decoder(Hq, Hk, Hv)
+        return logits
+
+
+
+if __name__ == '__main__':
+    import time
+    t1 = time.time()
+    batch_size  = 1
+    acoustic    = torch.rand(batch_size, 1000, 81)
+    linguistic  = torch.randint(1, 39, size=(batch_size, 40))
+    model       = AL(40)
+    o           = model(acoustic, linguistic)
+    t2 = time.time()
+    print(o)
+    print(o.shape)
+    print(t2 - t1)
